62c5ce3f8420a338543e7164cd9d4d1b36a43dc0
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-13 19:24:33 -0500

    Tuesday

--- Player.cs
+++ Player.cs
@@ -115,7 +115,51 @@ namespace gptgame
             int enemyHealth = 10;
             int playerHealth = enemyHealth + 2;
 
+            // Acceptance Criteria 2 & 3: Take turns and use skills/items
+            while ( enemyHealth > 0 && playerHealth > 0 )
+            {
+                Console.WriteLine("\nIt's your turn! Choose your action: [Attack | Heal | Stats]");
+                string action = Console.ReadLine();
+                Random enemyAttack = new Random();
+                int damage = enemyAttack.Next(5);
 
+                // Player turn
+                if ( action.ToLower() == "attack" )
+                {
+                    Console.WriteLine(value: $"You attacked the {chosenEnemy}");
+                    enemyHealth -= damage;
+                }
+                else if ( action.ToLower() == "heal" )
+                {
+                    Console.WriteLine("You healed yourself!");
+                    // Implement healing logic here
+                }
+                else if ( action.ToLower() == "stats" )
+                {
+                    //Console.WriteLine($"your stats are\n" +
+                    //$"current strength: {Player.strength}\n" +
+                    //$"current intelligence: {intelligence}\n" +
+                    //$"current agility: {agility}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid action. You lose your turn.");
+                }
+
+                // Enemy turn
+                Console.WriteLine($"The {chosenEnemy} attacks you!");
+                // Implement player damage logic here
+                Random playerAttack = new Random();
+                int playerDamage = playerAttack.Next(5);
+                playerHealth -= playerDamage;
+
+
+                // Display enemy health
+                Console.WriteLine($"Enemy Health: {enemyHealth}\n" +
+                    $"Player Health: {playerHealth}");
+            }
+
+            Console.WriteLine($"You have defeated the {chosenEnemy}!!!");
 
         }
     }
