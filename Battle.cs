09f2286974698fbbb31b89a55c5dcc108299122a
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-18 20:28:45 -0500

    created battle class

--- Battle.cs
+++ Battle.cs
@@ -4,13 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace gptgame
 {
     internal class Battle
     {
         public static void SequenceOne()
         {
-            Console.WriteLine("ASCII illustration of a dragon in flight with wings spread and the word 'Enemy' written underneath.");
+            
+
+            string lotrOne = @"                                             _______________________
+   _______________________-------------------                       `\
+ /:--__                                                              |
+||< > |                                   ___________________________/
+| \__/_________________-------------------                         |
+|                                                                  |
+ |                       THE LORD OF THE RINGS                      |
+ |                                                                  |
+ |       Three Rings for the Elven-kings under the sky,             |
+  |        Seven for the Dwarf-lords in their halls of stone,        |
+  |      Nine for Mortal Men doomed to die,                          |
+  |        One for the Dark Lord on his dark throne                  |
+  |      In the Land of Mordor where the Shadows lie.                 |
+   |       One Ring to rule them all, One Ring to find them,          |
+   |       One Ring to bring them all and in the darkness bind them   |
+   |     In the Land of Mordor where the Shadows lie.                |
+  |                                              ____________________|_
+  |  ___________________-------------------------                      `\
+  |/`--_                                                                 |
+  ||[ ]||                                            ___________________/
+   \===/___________________--------------------------";
+            Console.WriteLine(lotrOne);
+
+
+        }
+
+
+        public static void SequenceTwo()
+        {
+            string rolex1 = @"       ,--.-----.--.
+       |--|-----|--|
+       |--|     |--|
+       |  |-----|  |
+     __|--|     |--|__
+    /  |  |-----|  |  \
+   /   \__|-----|__/   \
+  /   ______---______   \/\
+ /   /  11  1 2 / 1  \   \/
+{   /10    ROLEX     2\   }
+|  {     ,_.    /  ,_. }  |-,
+|  |9  {   }  O--{- } 3|  | |
+|  {   `-'  /    `-'   }  |-'
+{   \8   7 /     5   4/   }
+ \   `------_6_------'   /\
+  \     __|-----|__     /\/
+   \   /  |-----|  \   /
+    \  |--|     |--|  /
+     --|  |-----|  |--
+       |--|     |--|
+       |--|-----|--|
+       `--'-----`--'";
+
+        Console.WriteLine(rolex1);
+
         }
 
 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace gptgame
 {
@@ -7,10 +8,14 @@ namespace gptgame
     {
         static void Main( string[] args )
         {
-
-            Player.Introduction();
-            Player.SelectPlayer();
-            Player.SelectRandomEnemy();
+            
+            //Player.Introduction();
+            //Player.SelectPlayer();
+            //Player.SelectRandomEnemy();
+            Battle.SequenceOne();
+            Thread.Sleep( 1000 );
+            Console.Clear();
+            Battle.SequenceTwo();
 
 
 
