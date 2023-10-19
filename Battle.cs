4eb8498007b0851b3f9998d3d49fad4c4b67615a
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-18 19:40:26 -0500

    looking for ascii art

---
+++ Battle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gptgame
+{
+    internal class Battle
+    {
+        public static void SequenceOne()
+        {
+            Console.WriteLine("ASCII illustration of a dragon in flight with wings spread and the word 'Enemy' written underneath.");
+        }
+
+
+
+    }
+}
